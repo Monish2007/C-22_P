7bea1267d09020c39ae5ae1bf202c3476b825317
Monish2007 <93634311+Monish2007@users.noreply.github.com>
2022-03-20 19:54:16 -0600

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,19 @@
+class Ground{
+
+    constructor(x, y, width, height, angle) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+       
+
+        display{}
+            
+            groundObj = new ground(width/2,670,width,20);
+            leftSide = new ground(1100,600,20,120);
+            rightSide = new ground(1500,600,20,120);
+        }
+      }
+
+
+
